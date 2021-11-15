55990c28ed4baa7afc7096efa1744f40a0fe9b9b
Amy <min.may.flow.2@gmail.com>
2021-11-15 16:37:03 -0500

    add basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddProf from "./components/add-prof";
 import Login from "./components/login";
 import Prof from "./components/prof";
 import Search from "./components/search";
 import SignUp from "./components/sign-up";
+import Landing from "./components/landing";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='professors/*' element={<Prof />} />
+        <Route path='login' element={<Login />} />
+        <Route path='signup' element={<SignUp />} />
+        <Route path='search' element={<Search />} />
+        <Route path='add' element={<AddProf />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
