37bfdcaae8f5122833be0b4dc405d187e43415ad
Amy <min.may.flow.2@gmail.com>
2021-12-01 11:42:10 -0500

    style login & signup pages

--- client/src/components/sign-up.js
+++ client/src/components/sign-up.js
@@ -1,8 +1,12 @@
-import React, {useState, useContext} from "react";
-import {useNavigate} from "react-router-dom";
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import Nav from "./nav";
-import axios from 'axios';
+import axios from "axios";
 import AuthContext from "./auth/authContext";
+import { TextField, Button, Select, MenuItem } from "@mui/material";
+import { FormStyle } from "../styles";
+
+const learningPreferences = ["Textbook", "Lecture"];
 
 function SignUp() {
   const [username, setUsername] = useState("");
@@ -12,66 +16,98 @@ function SignUp() {
   const [confirmPass, setConfirmPass] = useState("");
   const [errorDisplay, setErrorDisplay] = useState("none");
 
-  const {getLoggedIn} = useContext(AuthContext);
+  const { getLoggedIn } = useContext(AuthContext);
   let nav = useNavigate();
 
-  const divStyle = {
-    textAlign: "Center",
-    flexWrap: "wrap",
-    alignContent: "center"
-  };
-
   const formStyle = {
-    textAlign:"center"
-  }
-
+    textAlign: "center",
+  };
 
   async function trySignup(e) {
     e.preventDefault();
-    if(password !== confirmPass) {
+    if (password !== confirmPass) {
       alert("Passwords do not match");
     } else {
-
       try {
-
-        const login = await axios.post("http://localhost:8080/users/createUser", {username, email, learningPreference, password}, {withCredentials: true});
-        sessionStorage.setItem("user", JSON.stringify(login.data))
+        const login = await axios.post(
+          "http://localhost:8080/users/createUser",
+          { username, email, learningPreference, password },
+          { withCredentials: true }
+        );
+        sessionStorage.setItem("user", JSON.stringify(login.data));
         await getLoggedIn();
         nav("/search");
-
-      } catch(error) {
-
+      } catch (error) {
         setErrorDisplay("block");
-
       }
     }
   }
 
   return (
-    <div style={divStyle}>
-      <Nav status='login' />
-      <h1>SignUp!</h1>
-      <form onSubmit={trySignup} style={formStyle}>
-        <label htmlFor="username">Username:</label>
-        <input type="text" name="username" onChange={(e) => setUsername(e.target.value)} value={username} required />
-        <label htmlFor="email">Email:</label>
-        <input type="text" name="email" onChange={(e) => setEmail(e.target.value)} value={email} required /><br/><br/>
+    <div>
+      <Nav />
+      <div id='signup-content'>
+        <h1>Create Account</h1>
+        <form onSubmit={trySignup} style={formStyle}>
+          <TextField
+            required
+            name='username'
+            label='Username'
+            variant='outlined'
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+            sx={FormStyle}
+          />
+          <TextField
+            required
+            type='email'
+            name='email'
+            label='Email'
+            variant='outlined'
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            sx={FormStyle}
+          />
+          <Select
+            value={learningPreference}
+            onChange={(e) => setLearningPref(e.target.value)}
+            disableUnderline
+            sx={FormStyle}>
+            {learningPreferences.map((item) => (
+              <MenuItem key={item} value={item} sx={formStyle}>
+                {item}
+              </MenuItem>
+            ))}
+          </Select>
 
-        <label>Learning Preference:</label>
-        <select onChange={((e) => setLearningPref(e.target.value))}> 
-          <option value="Textbook">TextBook</option>
-          <option value="Lecture">Lecture</option>
-        </select>
-
-        <br/><br/>
-        
-        <label htmlFor="password">Password:</label>
-        <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} value={password} required />
-        <label htmlFor="cPassword">Confirm Password:</label>
-        <input type="password" name="cPassword" onChange={(e) => setConfirmPass(e.target.value)} value={confirmPass} required /><br/><br/>
-        <input type="submit" value="Submit" />
-      </form>
-      <span className="error" style={{display: errorDisplay, color: 'red'}}>That Username is Taken. Please try Another.</span>
+          <TextField
+            required
+            type='password'
+            name='password'
+            label='Password'
+            variant='outlined'
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            sx={FormStyle}
+          />
+          <TextField
+            required
+            type='password'
+            name='cPassword'
+            label='Confirm Password'
+            variant='outlined'
+            onChange={(e) => setConfirmPass(e.target.value)}
+            value={password}
+            sx={FormStyle}
+          />
+          <Button variant='contained' type='submit' sx={FormStyle}>
+            Sign Up
+          </Button>
+        </form>
+        <span className='error' style={{ display: errorDisplay, color: "red" }}>
+          That Username is Taken. Please try Another.
+        </span>
+      </div>
     </div>
   );
 }
---
+++ client/src/styles.js
@@ -0,0 +1,16 @@
+const FormStyle = {
+  "&.MuiButton-contained": {
+    backgroundColor: "#ffde6a",
+    maxWidth: "150px",
+    color: "black",
+    font: "inherit",
+    fontWeight: "600",
+    padding: "12px 30px",
+  },
+  "&.MuiOutlinedInput-root": {
+    textAlign: "left",
+    backgroundColor: "white",
+  },
+};
+
+export { FormStyle };
